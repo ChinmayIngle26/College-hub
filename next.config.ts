c3c2dc7057df322b4f455d0ddc226f44911d0355
Chinmay Ingle <chinmayingle26@gmail.com>
2025-06-08 10:42:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -24,7 +25,7 @@ const nextConfig: NextConfig = {
     ],
   },
   experimental: {
-    serverComponentsExternalPackages: ['firebase-admin'],
+    serverComponentsExternalPackages: ['firebase-admin', 'google-auth-library'],
   },
 };
 
